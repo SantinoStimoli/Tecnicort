ccb1ad0459671acbb227c19bc103fae10658093b
Santino Stimoli <107507086+santino-stimoli@users.noreply.github.com>
2023-05-05 11:37:17 -0300

    =$MES

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <h1>HOLAAA</h1>
+    <App />
   </React.StrictMode>
 )
